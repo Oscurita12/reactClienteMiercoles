4e2e778e2a39e6519b7262324f822b4fe0e809a9
206 <206@academicos.cesde.edu.co>
2022-06-01 09:12:48 -0500

    se crea cliente

---
+++ src/Clara.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+export function Clara(){
+
+    let universidad='CESDE'
+
+
+    return(
+        <>
+            <h1>Hola soy Clara</h1>
+            {/*<h4>Y soy tu amiga...</h4>*/}
+            <h5>Y estudio en {universidad}</h5>
+            <h6>y la suma de 5+1 es {5+1}</h6>
+        </>
+    )
+}
---
+++ src/Slider.js
@@ -0,0 +1,28 @@
+export function Slider () {
+
+    return (
+        <section>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img src="../assets/img/img1.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                    <img src="../assets/img/img2.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                    <img src="../assets/img/img3.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+    )
+}
